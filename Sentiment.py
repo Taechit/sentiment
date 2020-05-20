72b23ca816f27271ef266e52524962b0855dca81
Ongsa <ongsa.sungkanit@gmail.com>
2020-05-21 02:12:10 +0700

    verry good

--- Sentiment.py
+++ Sentiment.py
@@ -1,7 +1,8 @@
 from pythainlp.tokenize import word_tokenize
-text='ที่เปิดปีโป้เค้าเรียกว่า อะไรเหรอคะ ที่เป็นพลาสติก ปีโป้ เรียกยังไง'
+text='ที่เปิดปีโป้เค้าเรียกว่าหมู อะไรเหรอคะ ที่เป็นพลาสติก ปีโป้ เรียกยังไง'
 e=word_tokenize(text,engine='newmm') 
 for x in e : 
     if x == " ":
         e.remove(' ')
 print (e)  
+#moo
