1e3f0273cfca418e1c3f296009b02d924723e27e
Ongsa <ongsa.sungkanit@gmail.com>
2020-05-21 03:47:35 +0700

    checkRemove2

--- Sentiment.py
+++ Sentiment.py
@@ -21,16 +21,11 @@ for x in textPeepo :
     word = word_tokenize(x,engine='newmm')
     pattern = re.compile("[A-Za-z0-9/+*#!]+")
     for x in word :
-        checkRemove = False 
-        #print(x)
-        #print(pattern.search("555555555" ))
         if x == " ":
-            word.remove(' ') #ลบข่องว่างออก
-        if pattern.search(x): 
-            checkRemove = True
-        if len(x) >= 3:
+            word.remove(x) #ลบข่องว่างออก
+        elif pattern.search(x): 
+            word.remove(x)
+        elif len(x) >= 3:
             if x[len(x)-1] == x[len(x)-2] and x[len(x)-2] == x[len(x)-3]:
-                checkRemove = True           
-        if(checkRemove):word.remove(x)
-
-    #print(word)
+                word.remove(x)          
+    print(word)
