161cc4f480be336aeab6fb3eebbe4dd64f7a571c
Taechit <poomrape1704@hotmail.com>
2020-05-21 01:22:52 +0700

    new

--- Sentiment.py
+++ Sentiment.py
@@ -1,3 +1,7 @@
-import deepcut
-list_word = deepcut.tokenize('ตัดคำได้ดีมาก')
-print(list_word)
+from pythainlp.tokenize import word_tokenize
+text='ที่เปิดปีโป้เค้าเรียกว่า อะไรเหรอคะ ที่เป็นพลาสติก ปีโป้ เรียกยังไง'
+e=word_tokenize(text,engine='newmm') 
+for x in e : 
+    if x == " ":
+        e.remove(' ')
+print (e)  
