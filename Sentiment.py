d84af5377908a7cda7a4a1ab6fa8ca70ff175d65
Ongsa <ongsa.sungkanit@gmail.com>
2020-05-21 16:02:54 +0700

    moo training data2

--- Sentiment.py
+++ Sentiment.py
@@ -2,21 +2,36 @@ from pythainlp.tokenize import word_tokenize
 import codecs
 import re 
 
-text='ทั้งถุงมีสีขาวอันเดียว...คืออันเดียวจิงๆๆๆๆๆ ทำมัยยยยย  สีที่ไม่ชอบก็มีเยอะเกินนนนนนน'
-word=word_tokenize(text,engine='newmm') 
-for x in word : 
-    if x == " ":
-        word.remove(' ')
+#method สำหรับแยกคำ
+def stringSplit(txt):
+    pos = txt.rfind(',')
+    return txt[:pos-1],txt[pos+1:]
 
-#word.replace("ปี","เทส")
-#print (word)  
 
+#อ่านไฟล์ peepo
 with codecs.open('peepo', 'r', "utf-8") as f:
   lines = f.readlines()
-textPeepo=[e.strip() for e in lines]
+textPeepo=[x.strip() for x in lines]
 del lines
 f.close() # ปิดไฟล์
-#print(textPeepo)
+
+
+#อ่านไฟล์ training
+with codecs.open('trainingData', 'r', "utf-8") as f:
+  lines = f.readlines()
+training=[x.strip() for x in lines]
+del lines
+f.close() # ปิดไฟล์
+
+
+#training data
+trainingData = []
+for x in training:
+    string1,string2 = stringSplit(x)
+    trainingData.append((string1,string2))
+
+
+#
 for x in textPeepo :
     word = word_tokenize(x,engine='newmm')
     pattern = re.compile("[A-Za-z0-9/+*#!]+")
@@ -25,7 +40,9 @@ for x in textPeepo :
             word.remove(x) #ลบข่องว่างออก
         elif pattern.search(x): 
             word.remove(x)
-        elif len(x) >= 3:
+        elif len(x) >= 3:            
             if x[len(x)-1] == x[len(x)-2] and x[len(x)-2] == x[len(x)-3]:
                 word.remove(x)          
-    print(word)
+#print(trainingData[4][0])
+#x='หมู'
+#print(x.find('หมู'))
